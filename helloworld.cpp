dde754e8cb9761078c5695787ab0188f3ffcefa6
wadaniel <wadaniel@ethz.ch>
2023-09-24 20:36:52 +0200

    wip

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char** argv) {
     MPI_Init(NULL, NULL);  
     printf("helloworld %d\n", argc);
+    MPI_Comm comm_parent;
+    MPI_Comm_get_parent(&comm_parent);
+    MPI_Comm_disconnect(&comm_parent);
     MPI_Finalize();
     return 0;
 }
